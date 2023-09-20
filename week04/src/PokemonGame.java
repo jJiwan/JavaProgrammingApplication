201d0a0a4b2eed4cd13cf51377c1d4418d91433c
psangb <dksis12@g.shingu.ac.kr>
2023-09-20 14:53:26 +0900

    v19

---
+++ week04/src/PokemonGame.java
@@ -0,0 +1,10 @@
+
+public class PokemonGame {
+    public static void main(String[] args) {
+        NoFly noFly = new NoFly();
+        Pikachu pikachu = new Pikachu(noFly);
+
+        pikachu.performFly();
+
+    }
+}
